e6c80980f400219550dcd310f3c0698ab149acb0
Sanjay Pujari <sanjay@scopicsolutions.com>
2026-03-18 14:10:41 +0530

    Resolved build error

--- Backend/tests/Astronomy.MediaFactory.Tests/ExperimentApiEndpointsTests.cs
+++ Backend/tests/Astronomy.MediaFactory.Tests/ExperimentApiEndpointsTests.cs
@@ -1,6 +1,7 @@
 using Astronomy.MediaFactory.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net;
