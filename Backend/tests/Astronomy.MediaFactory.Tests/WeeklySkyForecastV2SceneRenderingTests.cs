9548a6d5f520edefe7808e708d4c5e5e16ea1c66
Sanjay-pujari <sanjay@scopicsolutions.com>
2026-05-26 16:46:03 +0530

    Fix missing StellariumBlueprintPackage argument in scene rendering test

--- Backend/tests/Astronomy.MediaFactory.Tests/WeeklySkyForecastV2SceneRenderingTests.cs
+++ Backend/tests/Astronomy.MediaFactory.Tests/WeeklySkyForecastV2SceneRenderingTests.cs
@@ -130,6 +130,7 @@ public sealed class WeeklySkyForecastV2SceneRenderingTests
                 EventIntelligence: [],
                 EventExtractionResult: null,
                 Storyboard: null,
+                StellariumBlueprintPackage: null,
                 WeeklyStoryArc: new WeeklyStoryArc("h","s","t","o",[],"c",[],[],[]),
                 EditorialStoryPackage: new WeeklyEditorialStoryPackage(new WeeklyHeroEvent("e","t","t","d",DateOnly.FromDateTime(DateTime.UtcNow),null,[],[],0,0,0,"v","w"),[],"h","s","o","t",[],[],new WeeklyThumbnailDirection([],[],[],"e","v","c","b","o"),[],"",[]),
                 CinematicStoryBlueprint: null,
