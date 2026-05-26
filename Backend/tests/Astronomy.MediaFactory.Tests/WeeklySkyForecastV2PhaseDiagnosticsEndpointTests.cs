682575dadfe98c996779959520a27ef42bdf5808
Sanjay Pujari <sanjay@scopicsolutions.com>
2026-05-26 16:25:49 +0530

    resplved the build issue

--- Backend/tests/Astronomy.MediaFactory.Tests/WeeklySkyForecastV2PhaseDiagnosticsEndpointTests.cs
+++ Backend/tests/Astronomy.MediaFactory.Tests/WeeklySkyForecastV2PhaseDiagnosticsEndpointTests.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using Astronomy.MediaFactory.Core;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Xunit;
 
