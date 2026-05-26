8a7e80e261a9272b900902922142688d33698686
Sanjay Pujari <sanjay@scopicsolutions.com>
2026-05-26 16:24:07 +0530

    Added reference

--- Backend/tests/Astronomy.MediaFactory.Tests/WeeklySkyForecastV2PhaseDiagnosticsEndpointTests.cs
+++ Backend/tests/Astronomy.MediaFactory.Tests/WeeklySkyForecastV2PhaseDiagnosticsEndpointTests.cs
@@ -2,6 +2,7 @@ using System.Net.Http.Json;
 using Astronomy.MediaFactory.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.TestHost;
+using Xunit;
 
 namespace Astronomy.MediaFactory.Tests;
 
