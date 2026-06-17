e613051f26648a7a54d4c94449fde2e8c07966f6
Sanjay Pujari <sanjay@scopicsolutions.com>
2026-06-17 11:40:06 +0530

    Applied db changes

---
+++ Backend/src/Astronomy.MediaFactory.Infrastructure/Persistence/Migrations/20260617060443_AddContentPlanManualValidationFlag.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Astronomy.MediaFactory.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContentPlanManualValidationFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                ALTER TABLE content_generation_plans ADD COLUMN IF NOT EXISTS manual_validation boolean NOT NULL DEFAULT FALSE;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                ALTER TABLE content_generation_plans DROP COLUMN IF EXISTS manual_validation;
+                """);
+        }
+    }
+}
